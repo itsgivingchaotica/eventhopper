1844b2f9086b59c5159ed1109e7644b4c6fe0117
Saoirse Siobhan Ebert <91578619+itsgivingchaotica@users.noreply.github.com>
2023-10-07 02:43:52 -0400

    update server

--- server/server.js
+++ server/server.js
@@ -1,36 +1,43 @@
-import express from 'express'
-import path from 'path'
-import favicon from 'serve-favicon'
-import dotenv from 'dotenv'
+import express from "express";
+import path from "path";
+import favicon from "serve-favicon";
+import dotenv from "dotenv";
+import eventsRouter from "./routes/events.js";
+import locationsRouter from "./routes/locations.js";
 
 // import the router from your routes file
 
+dotenv.config();
 
-dotenv.config()
+const PORT = process.env.PORT || 3000;
 
-const PORT = process.env.PORT || 3000
+const app = express();
 
-const app = express()
+app.use(express.json());
+app.use("/events", eventsRouter);
+app.use("/locations", locationsRouter);
 
-app.use(express.json())
+app.get("/", (req, res) => {
+  res
+    .status(200)
+    .send(
+      '<h1 style="text-align: center; margin-top: 50px;">Eventhopper API</h1>'
+    );
+});
 
-if (process.env.NODE_ENV === 'development') {
-    app.use(favicon(path.resolve('../', 'client', 'public', 'party.png')))
-}
-else if (process.env.NODE_ENV === 'production') {
-    app.use(favicon(path.resolve('public', 'party.png')))
-    app.use(express.static('public'))
+if (process.env.NODE_ENV === "development") {
+  app.use(favicon(path.resolve("../", "client", "public", "party.png")));
+} else if (process.env.NODE_ENV === "production") {
+  app.use(favicon(path.resolve("public", "party.png")));
+  app.use(express.static("public"));
 }
 
 // specify the api path for the server to use
 
-
-if (process.env.NODE_ENV === 'production') {
-    app.get('/*', (_, res) =>
-        res.sendFile(path.resolve('public', 'index.html'))
-    )
+if (process.env.NODE_ENV === "production") {
+  app.get("/*", (_, res) => res.sendFile(path.resolve("public", "index.html")));
 }
 
 app.listen(PORT, () => {
-    console.log(`server listening on http://localhost:${PORT}`)
-})
+  console.log(`server listening on http://localhost:${PORT}`);
+});
