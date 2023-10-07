da4578f4f161760d4aaf84028bf255bd9cbe5828
Saoirse Siobhan Ebert <91578619+itsgivingchaotica@users.noreply.github.com>
2023-10-07 18:53:26 -0400

    establish endpoints

--- server/server.js
+++ server/server.js
@@ -2,30 +2,24 @@ import express from "express";
 import path from "path";
 import cors from "cors";
 import favicon from "serve-favicon";
-import eventsRouter from "./routes/events.js";
-import locationsRouter from "./routes/locations.js";
+import { eventsRouter } from "./routes/events.js";
+import { locationsRouter } from "./routes/locations.js"; // Import default export
 import { pool } from "./config/database.js";
-
-// import the router from your routes file
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use("/events", eventsRouter);
 app.use("/locations", locationsRouter);
-
-// specify the api path for the server to use
+app.use("/events", eventsRouter);
 
 app.get("/", async (req, res) => {
   try {
     const resultsEvents = await pool.query("SELECT * FROM events");
     const resultsLocations = await pool.query("SELECT * FROM locations");
-    const responseData = {
-      html: '<h1 style="text-align: center; margin-top: 50px;">EventHopper API</h1>',
-      data: resultsLocations.rows.concat(resultsEvents.rows),
-    };
+    const responseData = resultsLocations.rows;
+    console.log(responseData);
     res.status(200).send(responseData);
   } catch (error) {
     res.status(409).json({ error: error.message });
@@ -36,6 +30,7 @@ if (process.env.NODE_ENV === "production") {
   app.get("/*", (_, res) => res.sendFile(path.resolve("public", "index.html")));
 }
 
+console.log(PORT, "PORT");
 app.listen(PORT, () => {
   console.log(`server listening on http://localhost:${PORT}`);
 });
