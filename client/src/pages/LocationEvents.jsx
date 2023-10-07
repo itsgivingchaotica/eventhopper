804b9867fcef3321bebb36f9d32ea2f728f653c4
Saoirse Siobhan Ebert <91578619+itsgivingchaotica@users.noreply.github.com>
2023-10-07 18:55:02 -0400

    begin pullign data for specific location events

--- client/src/pages/LocationEvents.jsx
+++ client/src/pages/LocationEvents.jsx
@@ -1,40 +1,76 @@
-import React, { useState, useEffect } from 'react'
-import Event from '../components/Event'
-import '../css/LocationEvents.css'
+import React, { useState, useEffect } from "react";
+import Event from "../components/Event";
+import EventsAPI from "../services/EventsAPI";
+import LocationsAPI from "../services/LocationsAPI";
+import "../css/LocationEvents.css";
 
-const LocationEvents = ({index}) => {
-    const [location, setLocation] = useState([])
-    const [events, setEvents] = useState([])
+const LocationEvents = ({ index }) => {
+  const [events, setEvents] = useState([]);
+  const [location, setLocation] = useState([]);
 
-    return (
-        <div className='location-events'>
-            <header>
-                <div className='location-image'>
-                    <img src={location.image} />
-                </div>
+  useEffect(() => {
+    (async () => {
+      try {
+        const locationEventsData = await EventsAPI.getAllEvents();
+        console.log(locationEventsData, "data");
+        console.log(index, "INDEX");
+        setEvents(locationEventsData);
+        const locationData = await LocationsAPI.getLocationById(index);
+        setLocation(locationData.data);
+        console.log(location);
+        location.filter((event) => event.venue !== location.name);
+        setLocation(location);
+        console.log(locationData, "locationdata");
+      } catch (error) {
+        console.error(error); // Log the error
+        // Handle the error here or rethrow it if needed
+      }
+    })();
+  }, []);
 
-                <div className='location-info'>
-                    <h2>{location.name}</h2>
-                    <p>{location.address}, {location.city}, {location.state} {location.zip}</p>
-                </div>
-            </header>
+  useEffect(() => {
+    if (events) {
+    }
+  }, [events]);
 
-            <main>
-                {
-                    events && events.length > 0 ? events.map((event, index) =>
-                        <Event
-                            key={event.id}
-                            id={event.id}
-                            title={event.title}
-                            date={event.date}
-                            time={event.time}
-                            image={event.image}
-                        />
-                    ) : <h2><i className="fa-regular fa-calendar-xmark fa-shake"></i> {'No events scheduled at this location yet!'}</h2>
-                }
-            </main>
+  return location ? (
+    <div className="location-events">
+      <header>
+        <div className="location-image">
+          <img src={`url/${location.image}`} />
         </div>
-    )
-}
 
-export default LocationEvents
+        <div className="location-info">
+          <h2>{location.name}</h2>
+          <p>
+            {location.address}, {location.city}, {location.state} {location.zip}
+          </p>
+        </div>
+      </header>
+
+      <main>
+        {events && events.length > 0 ? (
+          events.map((event, index) => (
+            <Event
+              key={event.id}
+              id={event.id}
+              title={event.name}
+              date={event.date}
+              time={event.time}
+              image={event.image}
+            />
+          ))
+        ) : (
+          <h2>
+            <i className="fa-regular fa-calendar-xmark fa-shake"></i>{" "}
+            {"No events scheduled at this location yet!"}
+          </h2>
+        )}
+      </main>
+    </div>
+  ) : (
+    <></>
+  );
+};
+
+export default LocationEvents;
