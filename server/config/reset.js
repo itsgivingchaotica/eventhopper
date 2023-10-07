d53489cf54f42d542b5355d6e2cb7e2115a3f87a
Saoirse Siobhan Ebert <91578619+itsgivingchaotica@users.noreply.github.com>
2023-10-07 02:33:04 -0400

    edit for locations

--- server/config/reset.js
+++ server/config/reset.js
@@ -1,6 +1,7 @@
 import { pool } from "./database.js";
 import "./dotenv.js";
 import eventData from "../data/events.js";
+import locationData from "../data/locations.js";
 
 const createEventsTable = async () => {
   const createTableQuery = `
@@ -10,10 +11,9 @@ const createEventsTable = async () => {
         id SERIAL PRIMARY KEY,
         name VARCHAR(255) NOT NULL,
         date DATE NOT NULL,
+        time TIME NOT NULL,
         venue VARCHAR(255) NOT NULL,
         description TEXT,
-        city VARCHAR(255) NOT NULL,
-        state CHAR(2) NOT NULL,
         category VARCHAR(255) NOT NULL,
         image VARCHAR(255) NOT NULL
     )
@@ -21,9 +21,33 @@ const createEventsTable = async () => {
 
   try {
     await pool.query(createTableQuery);
-    console.log("Created gifts table");
+    console.log("Created events table");
   } catch (error) {
-    console.log("⚠️ error creating gifts table", error);
+    console.log("⚠️ error creating events table", error);
+  }
+};
+
+const createLocationsTable = async () => {
+  const createTableQuery = `
+    DROP TABLE IF EXISTS locations;
+
+    CREATE TABLE IF NOT EXISTS locations (
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(255) NOT NULL,
+        address VARCHAR(255) NOT NULL,
+        city VARCHAR(255) NOT NULL,
+        state VARCHAR(255) NOT NULL,
+        zip VARCHAR(255) NOT NULL,
+        website VARCHAR(255) NOT NULL,
+        image VARCHAR(255) NOT NULL
+    )
+`;
+
+  try {
+    await pool.query(createTableQuery);
+    console.log("Created locations table");
+  } catch (error) {
+    console.log("⚠️ error creating locations table", error);
   }
 };
 
@@ -32,16 +56,15 @@ const seedEventsTable = async () => {
 
   eventData.forEach((event) => {
     const insertQuery = {
-      text: "INSERT INTO events (name, date, venue, description, city, state, category, image) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)",
+      text: "INSERT INTO events (name, date, time, venue, description, category, image) VALUES ($1, $2, $3, $4, $5, $6, $7)",
     };
 
     const values = [
       event.name,
       event.date,
+      event.time,
       event.venue,
       event.description,
-      event.city,
-      event.state,
       event.category,
       event.image,
     ];
@@ -56,4 +79,33 @@ const seedEventsTable = async () => {
   });
 };
 
+const seedLocationsTable = async () => {
+  await createLocationsTable();
+
+  locationData.forEach((location) => {
+    const insertQuery = {
+      text: "INSERT INTO locations (name, address, city, state, zip, website, image) VALUES ($1, $2, $3, $4, $5, $6, $7)",
+    };
+
+    const values = [
+      location.name,
+      location.address,
+      location.city,
+      location.state,
+      location.zip,
+      location.website,
+      location.image,
+    ];
+    pool.query(insertQuery, values, (err, res) => {
+      if (err) {
+        console.error("⚠️ error inserting event", err);
+        return;
+      }
+
+      console.log(`✅ ${location.name} added successfully`);
+    });
+  });
+};
+
 seedEventsTable();
+seedLocationsTable();
